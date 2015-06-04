7bd95b279313e64227a79b03f7a005683d073f3b
Tony Yip <tony@opensource.hk>
2015-06-04 16:34:19 +0800

    First Try

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Routing\Controller;
+
+class AuthController extends Controller {
+
+	/**
+	 * @return Response
+	 */
+	public function authenticate()
+	{
+		#if (Auth::
+	}
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,9 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', function() {
+	return view('index.view');
+});
 
 Route::get('home', 'HomeController@index');
 
