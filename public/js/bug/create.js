ba4974560c5ffaf14531568f87c4ddb5e2ab6236
Tony Yip <tony@opensource.hk>
2015-07-09 09:41:17 +0800

    Create Bug Report Page View

--- public/js/bug/create.js
+++ public/js/bug/create.js
@@ -2,12 +2,8 @@
 	var colors = {};
 
 	function level_color(){
-		var level = $(document.getElementById('level')).val(), color = colors[level] || 'ffffff';
-		$(this).css({
-			'color' : 'white',
-			'background-color' : '#' + color,
-			'font-weight' : 'bold'
-		});
+		var level = $(document.getElementById('level')).val(), color = colors[level] || '000000';
+		$(this).css('background-color', '#' + color);
 	}
 	
 	$.getJSON('/ajax/bug/color', function (data) {
@@ -16,4 +12,20 @@
 	});
 
 	$(document.getElementById('level')).bind('change', level_color);
+
+	$('.nav-tabs a').bind('click', function (e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+
+	$('a[aria-controls=preview]').bind('show.bs.tab', function (e) {
+		var content = $('textarea').val() || '#Nothing to preview', t = $(document.getElementById('preview'));
+		t.css({
+			'height': '174px',
+			'border': '1px solid #cccccc',
+			'box-shadow': 'inset 0 1px 1px rgba(0,0,0,.075)',
+			'border-radius': '5px'
+		});
+		t.html(markdown.toHTML(content));
+	});
 }(jQuery);
--- resources/views/bug/create.blade.php
+++ resources/views/bug/create.blade.php
@@ -14,11 +14,45 @@
         <div class="form-group">
             <label class="col-sm-2 control-label" for="level">Level</label>
             <div class="col-sm-7">
-                <select id="level" name="level" class="form-control" style="background-color: #FBCA04">
+                <select id="level" name="level" class="form-control" style="background-color: #FBCA04; font-weight: bold; color: #FFFFFF;">
                     @foreach($levels as $level)
                         <option value="{{$level}}">{{$level}}</option>
                     @endforeach
                 </select>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-sm-2 control-label" for="page">Page Link</label>
+            <div class="col-sm-7">
+                <input id="page" name="page" class="form-control" placeholder="Page Link" type="url">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-sm-2 control-label" for="detail">Detail</label>
+            <div class="col-sm-7">
+                <ul class="nav nav-tabs" role="tablist">
+                    <li role="presentation" class="active">
+                        <a  href="#edit" aria-controls="edit" aria-expanded="true" role="tab" data-toggle="tab">Write</a>
+                    </li>
+                    <li role="presentation">
+                        <a href="#preview" aria-controls="preview" aria-expanded="true" role="tab" data-toggle="tab">Preview</a>
+                    </li>
+                </ul>
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane active" id="edit">
+                        <textarea name="detail" id="detail" class="form-control" rows="8"></textarea>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="preview"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-7">
+                <button type="submit" class="btn btn-danger">Submit</button>
+                <button type="reset" class="btn btn-primary">Reset</button>
             </div>
         </div>
 
@@ -28,5 +62,6 @@
 
 @section('scripts')
     @parent
+    <script src="/js/markdown.min.js"></script>
     <script src="/js/bug/create.js"></script>
 @stop
