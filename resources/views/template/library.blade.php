7feb590ea1828eb24193191a6d017fafacbe7856
Tony Yip <tony@opensource.hk>
2015-07-24 10:53:11 +0800

    Add views

---
+++ resources/views/template/library.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('stylesheet')
+    @parent
+    <link href="/css/library/style.css" rel="stylesheet" type="text/css">
+@stop
+
+@section('navbar')
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ route('library.index') }}">Library</a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="http://localhost:8080/bug">Bug Report</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@stop
