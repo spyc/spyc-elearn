8f8bec4eefde59d130e72d8f138556b55fd70941
Tony Yip <tony@opensource.hk>
2015-07-28 22:23:52 +0800

    Move File Name

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('stylesheet')
+    @parent
+    <link rel="stylesheet" href="{{ url('/css/signin.css') }}" type="text/css">
+@stop
+
+@section('wrap')
+<div class="container-fluid">
+	<form class="form-signin" method="POST">
+        <h2 class="form-signin-heading">Please Sign in</h2>
+        <label for="username" class="sr-only">PYC ID</label>
+        <input type="text" name="pycid" id="username" class="form-control" placeholder="PYC ID" autofocus required>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+        {!! csrf_field() !!}
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+</div>
+@endsection
