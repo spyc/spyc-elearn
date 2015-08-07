1a369387667bce348f31283d81a78e6d5916d364
Tony Yip <tony@opensource.hk>
2015-08-07 16:56:09 +0800

    Add log:clear command

---
+++ app/Commands/Log/LogClearCommand.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * elearn
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Tony Yip 2015.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category Guardian
+ * @author   Tony Yip <tony@opensource.hk>
+ * @license  http://opensource.org/licenses/GPL-3.0 GNU General Public License
+ */
+
+namespace App\Commands\Log;
+
+
+use App\Commands\Command;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Filesystem\Filesystem;
+
+class LogClearCommand extends Command implements SelfHandling
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'log:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove Log';
+
+    /**
+     * The filesystem instance.
+     *
+     * @var Filesystem
+     */
+    protected $files;
+
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+        $this->files = $files;
+    }
+
+    public function handle()
+    {
+        $logs = $this->files->glob(storage_path('logs'));
+
+        foreach ($logs as $log) {
+            $this->files->delete($log);
+        }
+
+        $this->info('Clear Log Success!');
+    }
+}
