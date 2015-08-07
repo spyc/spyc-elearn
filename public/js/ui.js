7cff713e2d20c272480dacf30064365ef9bfb376
Tony Yip <tony@opensource.hk>
2015-08-07 12:02:31 +0800

    Use API Server

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,6 +1,6 @@
 !function ($) {
     if ($('.tag-label[data-level]').length) {
-        $.getJSON('/ajax/bug/color', function (data) {
+        $.getJSON('/api/bug/level', function (data) {
             $('.tag-label[data-level]').each(function () {
                 var level = $(this).attr('data-level');
                 $(this).css('background-color', '#' + data[level]);
