d6354fbfe8af96a941671c1f04a16e9906941f38
Tony Yip <tony@opensource.hk>
2015-08-14 11:33:50 +0800

    Add library news table

---
+++ database/migrations/2015_08_14_033137_create_library_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLibraryNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('library_news', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('title', 64)->index();
+            $table->text('content')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('library_news');
+    }
+}
