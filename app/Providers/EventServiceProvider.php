0692a6510bcca0bba793ca94920d0c0dfac6bca5
Tony Yip <tony@opensource.hk>
2015-09-09 21:26:00 +0800

    Add SSO Event Service Listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Aacotroneo\Saml2\Events\Saml2LoginEvent;
+use App\Listeners\SSOLoginListener;
+use Aacotroneo\Saml2\Events\Saml2LogoutEvent;
+use App\Listeners\SSOLogoutListener;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -16,6 +20,12 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\SomeEvent' => [
             'App\Listeners\EventListener',
         ],
+        Saml2LoginEvent::class => [
+            SSOLoginListener::class
+        ],
+        Saml2LogoutEvent::class => [
+            SSOLogoutListener::class
+        ]
     ];
 
     /**
