4547b53a3267edd0a992f41e582625d8ecc5fa70
Tony Yip <tony@opensource.hk>
2015-10-14 22:40:56 +0800

    Change Countdown layout

--- resources/views/countdown.blade.php
+++ resources/views/countdown.blade.php
@@ -8,24 +8,58 @@
         </div>
     </div>
     <div class="container">
-        <div class="panel panel-success">
-            <div class="panel-heading lead">{{ trans('calendar.christmas') }}</div>
-            <div class="panel-body h4">
-                <span data-countdown="2015-12-24T16:00:00"></span>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="panel panel-green">
+                    <div class="panel-heading lead">
+                        <div class="row">
+                            <div class="col-xs-12 text-left">
+                                <div class="huge">{{ trans('calendar.christmas') }}</div>
+                                <div data-countdown="2015-12-24T16:00:00"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer h4">
+                        The Day of Jesus born
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div class="panel panel-danger">
-            <div class="panel-heading lead">{{ trans('calendar.first_term') }} {{ trans('calendar.exam') }}</div>
-            <div class="panel-body h4">
-                <span data-countdown="2016-01-04T00:30:00"></span>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="panel panel-red">
+                    <div class="panel-heading lead">
+                        <div class="row">
+                            <div class="col-xs-12 text-left">
+                                <div class="huge">
+                                    {{ trans('calendar.first_term') }} {{ ucfirst(trans('calendar.exam')) }}
+                                </div>
+                                <div data-countdown="2016-01-04T00:30:00"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer h4">
+                        First Term Exam for all S1 to S5 Students
+                    </div>
+                </div>
             </div>
-        </div>
-
-        <div class="panel panel-danger">
-            <div class="panel-heading lead">{{ trans('calendar.dse') }} {{ trans('calendar.exam') }} (DSE)</div>
-            <div class="panel-body h4">
-                <span data-countdown="2016-04-05T00:30:00"></span>
+            <div class="col-lg-4 col-md-6">
+                <div class="panel panel-red">
+                    <div class="panel-heading lead">
+                        <div class="row">
+                            <div class="col-xs-12 text-left">
+                                <div class="huge">
+                                    DSE {{ ucfirst(trans('calendar.exam')) }}
+                                </div>
+                                <div data-countdown="2016-04-05T00:30:00"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer h4">
+                        DSE Exam for all S6 Students
+                    </div>
+                </div>
             </div>
         </div>
     </div>
